01f6da110c3049a60e8db3af6339a257c44f3815
foorcun <msnifoorcun@hotmail.com>
2020-04-12 01:52:51 +0300

    Add files via upload

---
+++ state.js
@@ -0,0 +1,24 @@
+class State {
+    constructor(no) {
+      this.no = no;
+
+
+
+
+    }
+    actionNorth(newState) {
+      return newState;
+    }
+
+    actionSouth(newState) {
+        return newState;
+      }
+
+      actionEast(newState) {
+        return newState;
+      }
+
+      actionWest(newState) {
+        return newState;
+      }
+  }
